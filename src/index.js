b4fdcb9b1d23b08817a4941c5d66080edec66e97
Evgeniia Garnaga <evgarnaga@gmail.com>
2020-10-01 11:48:28 +0600

    feat: add min function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+
+    return 0;
+  } else {
+    let arr = array;
+    arr.sort((a, b) => a - b);
+    let minimalValue = arr[0];
+    
+    return minimalValue;
+  }
 }
 
 exports.max = function max (array) {
